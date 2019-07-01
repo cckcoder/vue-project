10eb6c53eda8e2a877958ca2db801d9b0e5e0a3c
dev2 <dev2@dev2.debian>
2019-07-01 15:06:01 +0700

    lession3_css

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,50 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="main">
+		<ul>
+			<li v-for="todo in todos" :key="todo.time" :style="{color: color}">
+				{{todo.text}}
+			</li>
+		</ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+  name: 'main',
+	filters: {
+		capitalize (val) {
+			return val.toUpperCase()
+		}
+	},
+	data () {
+		return {
+			todos: [
+				{
+					text: 'Todo1',
+					time: 1540352507,
+					completed: true
+				}
+				,{
+					text: 'Todo2',
+					time: 1540352506,
+					completed: true
+				}
+				,{
+					text: 'Todo3',
+					time: 1540352508,
+					completed: true
+				}
+				,{
+					text: 'Todo4',
+					time: 1540352607,
+					completed: false
+				}
+			],
+			srcUrl: "www.google.co.th",
+			color: 'red'
+		}
+	}
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
